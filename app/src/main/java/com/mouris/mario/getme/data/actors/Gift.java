fe30279b980fb42016151106367cbbc46b11457a
Mario Amgad <marioamgad9@gmail.com>
2018-04-16 18:54:24 +0200

    Add buyer_id to Gift

--- app/src/main/java/com/mouris/mario/getme/data/actors/Gift.java
+++ app/src/main/java/com/mouris/mario/getme/data/actors/Gift.java
@@ -2,14 +2,16 @@ package com.mouris.mario.getme.data.actors;
 
 public class Gift extends Actor {
 
-    public String gift_name, description, web_link, brand;
+    public String gift_name, description, web_link, brand, buyer_id;
 
     public Gift() { }
 
-    public Gift(String gift_name, String web_link, String brand, String description) {
+    public Gift(String gift_name, String web_link,
+                String brand, String description, String buyer_id) {
         this.gift_name = gift_name;
         this.web_link = web_link;
         this.brand = brand;
         this.description = description;
+        this.buyer_id = buyer_id;
     }
 }
