311408afaafda492dce3040d761a92a60f043c69
Mario Amgad <marioamgad9@gmail.com>
2018-04-17 13:10:38 +0200

    Change Wishlist background

---
+++ app/src/main/java/com/mouris/mario/getme/ui/wishlist_detail_activity/WishlistDetailActivity.java
@@ -0,0 +1,15 @@
+package com.mouris.mario.getme.ui.wishlist_detail_activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mouris.mario.getme.R;
+
+public class WishlistDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wishlist_detail);
+    }
+}
